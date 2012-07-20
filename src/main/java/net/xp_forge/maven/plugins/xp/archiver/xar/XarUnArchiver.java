d6967ec00ef4fcf66845af3eee1131b13b71bdd7
Marius Rosoiu <marius.rosoiu@1and1.ro>
2012-07-20 18:25:53 +0300

    Avoid parameter assignment

--- src/main/java/net/xp_forge/maven/plugins/xp/archiver/xar/XarUnArchiver.java
+++ src/main/java/net/xp_forge/maven/plugins/xp/archiver/xar/XarUnArchiver.java
@@ -83,16 +83,17 @@ public class XarUnArchiver extends AbstractUnArchiver {
   @Override
   protected void execute(String path, File outputDirectory) throws ArchiverException {
     File srcFile= this.getSourceFile();
-    //getLogger().debug("Expanding [" + srcFile + "#" + path + "] into [" + outputDirectory + "]");
+    String archivePath= path;
+    //getLogger().debug("Expanding [" + srcFile + "#" + archivePath + "] into [" + outputDirectory + "]");
 
     // Remove starting slash from path; if the case
-    if (path.startsWith("/")) {
-      path= path.substring(1);
+    if (archivePath.startsWith("/")) {
+      archivePath= archivePath.substring(1);
     }
 
     // Remove trailing slash from path; if the case
-    if (path.endsWith("/")) {
-      path= path.substring(0, path.length() - 1);
+    if (archivePath.endsWith("/")) {
+      archivePath= archivePath.substring(0, archivePath.length() - 1);
     }
 
     // Load archive
@@ -107,13 +108,13 @@ public class XarUnArchiver extends AbstractUnArchiver {
       outputDirectory.mkdirs();
     }
 
-    // Extract archive entries that start with path
+    // Extract archive entries that start with archivePath
     File outFile;
     for (XarEntry entry : archive.getEntries()) {
       String entryName= entry.getName();
 
       // Full entry match
-      if (entryName.equals(path)) {
+      if (entryName.equals(archivePath)) {
         int pos= entryName.lastIndexOf('/');
         if (-1 == pos) {
           outFile= new File(outputDirectory, entryName);
@@ -122,8 +123,8 @@ public class XarUnArchiver extends AbstractUnArchiver {
         }
 
       // Directory match
-      } else if (entryName.startsWith(path + "/")) {
-        outFile= new File(outputDirectory, entryName.substring(path.length() + 1));
+      } else if (entryName.startsWith(archivePath + "/")) {
+        outFile= new File(outputDirectory, entryName.substring(archivePath.length() + 1));
 
       // No match; try next entry
       } else {
